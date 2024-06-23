42f6c8356da7b8a213bbaaeef6921a2d35723f69
Tyrel Chambers <tychambers3@gmail.com>
2024-06-22 20:40:39 -0400

    filter search on completed

--- src/pages/dashboard/stories/completed.tsx
+++ src/pages/dashboard/stories/completed.tsx
@@ -1,8 +1,9 @@
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import EmptyState from "~/components/EmptyState";
 import StoryListItem from "~/components/StoryListItem";
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import WrapperWithNav from "~/layouts/WrapperWithNav";
 import { storiesTabs } from "~/routes";
 import { MixpanelEvents } from "~/types";
@@ -18,6 +19,8 @@ const Completed = () => {
       apiContext.story.getCompletedList.invalidate();
     },
   });
+  const [query, setQuery] = useState("");
+  const regex = new RegExp(query, "gi");
 
   return (
     <WrapperWithNav tabs={storiesTabs}>
@@ -30,25 +33,38 @@ const Completed = () => {
             </p>
           </div>
 
-          <Button
-            variant="secondary"
-            className="mt-4 lg:mt-0"
-            onClick={() => {
-              trackUiEvent(MixpanelEvents.REMOVE_ALL_COMPLETED_STORIES, {
-                userId: data?.user?.id,
-              });
-              removeAll.mutate();
-            }}
-          >
-            Remove all
-          </Button>
+          <div className="flex w-full max-w-md flex-col gap-3 lg:flex-row">
+            <div className="flex flex-1 flex-col">
+              <Input
+                placeholder="Search by keywords"
+                value={query}
+                onChange={(e) => setQuery(e.currentTarget.value)}
+              />
+            </div>
+            <Button
+              variant="secondary"
+              className="mt-4 lg:mt-0"
+              onClick={() => {
+                trackUiEvent(MixpanelEvents.REMOVE_ALL_COMPLETED_STORIES, {
+                  userId: data?.user?.id,
+                });
+                removeAll.mutate();
+              }}
+            >
+              Remove all
+            </Button>
+          </div>
         </header>
 
         {completedListQuery.data && completedListQuery.data.length > 0 ? (
           <div className="mt-6 grid grid-cols-2 gap-3">
-            {completedListQuery.data?.map((item) => (
-              <StoryListItem key={item.id} story={item} list="completed" />
-            )) || null}
+            {completedListQuery.data
+              ?.filter(
+                (item) => item.title.match(regex) || item.author.match(regex),
+              )
+              .map((item) => (
+                <StoryListItem key={item.id} story={item} list="completed" />
+              )) || null}
           </div>
         ) : (
           <EmptyState label="completed stories" />

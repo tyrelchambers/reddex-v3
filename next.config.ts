101ecc1a83fb142aa3b7fb953308cbf77db40336
Tyrel Chambers <tychambers3@gmail.com>
2024-12-15 13:58:21 -0500

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const config = {
       },
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   output: "standalone",
   webpack(config: any) {
     config.module.rules.push({

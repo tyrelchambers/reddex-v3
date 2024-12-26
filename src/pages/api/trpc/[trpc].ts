71c8284377e1e302bf53dff77b28e4060e2373ac
Tyrel Chambers <tychambers3@gmail.com>
2024-12-26 15:06:33 -0500

    capture trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,17 +3,19 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env";
 import { createTRPCContext } from "~/server/api/trpc";
 import { appRouter } from "~/server/api/root";
+import { captureException } from "@sentry/node";
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
+  onError({ path, error }) {
+    if (env.NODE_ENV === "development") {
+      console.error(
+        `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+      );
+    } else {
+      captureException(error);
+    }
+  },
 });
